434bc1fb092f86dd89f6e8a8dc905352450d21ae
Tanner Silva <t@tannersilva.com>
2021-07-31 04:01:14 +0000

    added three types of external scripts that will be used in the initial test suite for this project

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,6 +2,31 @@ import XCTest
 
 import SwiftSlashTests
 
+let testWriteScript = """
+#! swift path
+import Glibc
+print(readLine()!)
+exit(0)
+"""
+
+let testExitScript = """
+#! swift path
+import Glibc
+let randomInt = Int32.random(in:1...255)
+print(randomInt)
+exit(randomInt)
+"""
+
+let testReadScript = """
+#! swift path
+for i in 0..<10000 {
+	print("\(i)")
+}
+defer {
+	print("ex")
+}
+"""
+
 var tests = [XCTestCaseEntry]()
 tests += SwiftSlashTests.allTests()
 XCTMain(tests)
