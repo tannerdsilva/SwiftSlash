ffdc1e2f35864d3f015fa55ab3085690ce132ae9
tannerdsilva <t@tannersilva.com>
2021-08-18 23:06:27 +0000

    Added internal typealias ProcessTerminationHandler

--- Sources/SwiftSlash/Framework Internals/HandlerTypes.swift
+++ Sources/SwiftSlash/Framework Internals/HandlerTypes.swift
@@ -3,3 +3,4 @@ import Foundation
 internal typealias InboundDataHandler = (Data) -> Void
 internal typealias OutboundDataHandler = () -> Data
 internal typealias DataChannelTerminationHander = () -> Void
+internal typealias ProcessTerminationHandler = (Int32?) -> Void
