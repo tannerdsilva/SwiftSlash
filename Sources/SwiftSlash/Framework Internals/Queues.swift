6a2762e7a043c7e7c386d77d222e0a6e731ab9c9
tannerdsilva <t@tannersilva.com>
2021-10-14 05:54:35 +0000

    deleted Queues.swift because DispatchQueues are no longer used for internal synchronization in this framework

--- Sources/SwiftSlash/Framework Internals/Queues.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-
-internal let process_master_queue = DispatchQueue(label:"com.swiftslash.global.process", attributes:[.concurrent])
-internal let file_handle_guard = DispatchQueue(label:"com.swiftslash.global.filehandle.sync", target:process_master_queue)
