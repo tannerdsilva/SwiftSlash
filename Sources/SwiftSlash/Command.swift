5d8d475175be5c2158f7a9ef35f2f660abf85637
Tanner Silva <t@tannersilva.com>
2021-11-29 00:06:17 -0600

    CommandResult is now Command.Result Command(command:String) has been changed to a nameless initializer Command(_:String)

--- Sources/SwiftSlash/Command.swift
+++ Sources/SwiftSlash/Command.swift
@@ -2,15 +2,34 @@ import Foundation
 
 /// Defines a command that is to be executed.
 public struct Command:Hashable, Equatable {
-    
+    /// Contains the result of a `Command` that has run synchronously
+    public struct Result {
+        /// A convenience boolean that is set to `true` when `exitCode` is set to `0`
+        public let succeeded:Bool
+        public let exitCode:Int32
+        public let stdout:[Data]
+        public let stderr:[Data]
+        
+        internal init(exitCode:Int32, stdout:[Data], stderr:[Data]) {
+            self.exitCode = exitCode
+            self.stdout = stdout
+            self.stderr = stderr
+            if exitCode == 0 {
+                self.succeeded = true
+            } else {
+                self.succeeded = false
+            }
+        }
+    }
+
 	public var executable:String
 	public var arguments:[String]
 	public var environment:[String:String] = CurrentProcessState.getCurrentEnvironmentVariables()
     public var workingDirectory:URL = CurrentProcessState.getCurrentWorkingDirectory()
     
-    /// Initialize from a string-encoded command
+    /// Initialize from a string-encoded command, with spaces separating the arguments.
     /// - Parameter command: A string representing an executable (and optionally, the arguments following the executable) that you would like to run. Arguments separated by space
-	public init(command:String) {
+	public init(_ command:String) {
 		guard command.count > 0 else {
 			fatalError("cannot cannot initialize with a string of zero length")
 		}
@@ -39,8 +58,8 @@ public struct Command:Hashable, Equatable {
 	}
     
     /// Run a command synchronously
-    /// - Returns: Results of the command are captured in a `CommandResult` and returned after the command has finished executing
-	public func runSync() async throws -> CommandResult {
+    /// - Returns: Results of the command are captured in a ``Result`` and returned after the command has finished executing
+    public func runSync() async throws -> Command.Result {
 		let procInterface = ProcessInterface(command:self)
 		try await procInterface.launch()
 		//add the stdout task
@@ -55,7 +74,7 @@ public struct Command:Hashable, Equatable {
 			errLines.append(line)
 		}
 		let exitCode = try await procInterface.exitCode()
-		return CommandResult(exitCode:exitCode, stdout:outLines, stderr:errLines)
+		return Result(exitCode:exitCode, stdout:outLines, stderr:errLines)
 	}
 	
 	public static func == (lhs:Command, rhs:Command) -> Bool {
@@ -67,24 +86,4 @@ public struct Command:Hashable, Equatable {
 		hasher.combine(arguments)
 		hasher.combine(environment)
 	}
-}
-
-/// Contains the result of a `Command` that has run synchronously
-public struct CommandResult {
-    /// A convenience boolean that is set to `true` when `exitCode` is set to `0`
-    public let succeeded:Bool
-	public let exitCode:Int32
-	public let stdout:[Data]
-	public let stderr:[Data]
-    
-    internal init(exitCode:Int32, stdout:[Data], stderr:[Data]) {
-        self.exitCode = exitCode
-        self.stdout = stdout
-        self.stderr = stderr
-        if exitCode == 0 {
-            self.succeeded = true
-        } else {
-            self.succeeded = false
-        }
-    }
 }
