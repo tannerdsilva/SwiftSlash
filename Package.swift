5905b80b489819291d9a2b3cb9d978252201c7fa
Tanner Silva <t@tannersilva.com>
2021-12-01 11:53:42 -0600

    ClibSwiftSlash is a private library

--- Package.swift
+++ Package.swift
@@ -12,10 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftSlash",
-            targets: ["SwiftSlash"]),
-        .library(
-        	name: "ClibSwiftSlash",
-        	targets: ["ClibSwiftSlash"]),
+            targets: ["SwiftSlash"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
