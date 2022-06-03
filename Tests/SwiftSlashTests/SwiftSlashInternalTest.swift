15f0adfed33b7667b8c7196545ad314453d567ca
Tanner Silva <t@tannersilva.com>
2022-06-02 22:39:24 -0500

    basic tests in place for clibswiftslash

--- Tests/SwiftSlashTests/SwiftSlashInternalTest.swift
+++ Tests/SwiftSlashTests/SwiftSlashInternalTest.swift
@@ -19,4 +19,49 @@ final class SwiftSlashInternalTests:XCTestCase {
 		}
 		XCTAssert((getUsed + 1) == newUsed)
 	}
+	
+	func testLineParse() throws {
+		let lineTestData = [Data("this is a test of the first line".utf8), Data("".utf8), Data("this is a test of the third line".utf8), Data("line4".utf8)];
+		var patternData = Data()
+		patternData.append(10)
+		patternData.append(13)
+		var lineDataMerged = Data(lineTestData.joined(separator:patternData))
+		for curThing in lineDataMerged.enumerated() {
+			print("\(curThing.0) - \(curThing.1)")
+		}
+		var buildThings = [Data]()
+		var myLP = lineparser();
+		withUnsafePointer(to:&buildThings) { unsafePointer in
+			patternData.withUnsafeMutableBytes({ patternIn in
+				lp_init(&myLP, patternIn.baseAddress, UInt8(patternIn.count), UnsafeMutableRawPointer(mutating:unsafePointer)) { buff, buffSize, usrPtr in
+					let intakeData = Data(bytes:buff!, count:buffSize)
+					usrPtr!.assumingMemoryBound(to:[Data].self).pointee.append(intakeData)
+				}
+				lineDataMerged.withUnsafeBytes({ dataIn in
+					lp_intake(&myLP, dataIn.baseAddress, dataIn.count);
+				})
+				lp_close(&myLP);
+			})
+			guard lineTestData == unsafePointer.pointee else {
+				print("\(unsafePointer.pointee)")
+				XCTFail()
+				return
+			}
+		}
+	}
+	
+	func testQueue() throws {
+		var et = et_alloc()!;
+		et_init(et);
+		let newPipe = try PosixPipe(nonblockingReads:true, nonblockingWrites:true);
+		et_r_register(et, newPipe.reading) { fh, readSize, isClosed in
+			print("attempting to read \(readSize) bytes")
+		}
+		let writeString = "hello this is a test\n"
+		write(newPipe.writing, writeString, writeString.count);
+		sleep(5);
+		print("trying to close")
+		et_close(et);
+		print("close completed")
+	}
 }
