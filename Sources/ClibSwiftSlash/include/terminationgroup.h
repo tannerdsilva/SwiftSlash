d30aec85ddc151391d996a791fefb8f3b3dac804
Tanner Silva <t@tannersilva.com>
2022-06-23 01:46:12 -0500

    improved atomic procedures

--- Sources/ClibSwiftSlash/include/terminationgroup.h
+++ Sources/ClibSwiftSlash/include/terminationgroup.h
@@ -14,14 +14,18 @@
 
 // various states that a process can be in
 typedef enum processstatus {
-	launching = 1,
-	running = 2,
-	exited = 3,
-	signaled = 4
+	tg_launching = 0,
+	tg_running = 1,
+	tg_exited = 2,
+	tg_signaled = 3,
+	tg_aborted = 4
 } processstatus_t;
 
+// pid pointer type
+typedef pid_t*_Nullable pid_ptr_t;
+
 // termination handler: used by the termination group to notify when a process is done running
-typedef void(*_Nonnull terminationhandler)(usr_ptr_t, pid_t, int);
+typedef void(*_Nonnull terminationhandler)(usr_ptr_t, pid_t, processstatus_t, int);
 
 /*
  termination group
@@ -33,9 +37,9 @@ typedef void(*_Nonnull terminationhandler)(usr_ptr_t, pid_t, int);
 typedef struct terminationgroup {
 	_Atomic uint8_t status;
 	pid_t pid;
-	int exitCode;
-	uint count;
-	usr_ptr_t usrPtr;
+	_Atomic int exitCode;
+	_Atomic uint count;
+	_Atomic usr_ptr_t usrPtr;
 	terminationhandler th;
 } terminationgroup_t;
 typedef terminationgroup_t*_Nonnull terminationgroup_ptr_t;
@@ -50,6 +54,7 @@ int tg_decrement(terminationgroup_ptr_t);
 int tg_launch(terminationgroup_ptr_t, pid_t);
 int tg_exit(terminationgroup_ptr_t, int);
 int tg_signal(terminationgroup_ptr_t, int);
+int tg_abort(terminationgroup_ptr_t);
 
 void*_Nullable tg_pid_ptr(terminationgroup_ptr_t);
 
--- Sources/ClibSwiftSlash/terminationgroup.c
+++ Sources/ClibSwiftSlash/terminationgroup.c
@@ -8,105 +8,110 @@
 #include "terminationgroup.h"
 #include <stdatomic.h>
 #include <stdlib.h>
+#include <string.h>
 
 terminationgroup_ptr_t tg_init(usr_ptr_t usrPtr, terminationhandler th) {
 	terminationgroup_ptr_t tgp = malloc(sizeof(terminationgroup_t));
-	tgp->count = 0;
 	tgp->th = th;
-	tgp->usrPtr = usrPtr;
-	atomic_store(&tgp->status, launching);
+	atomic_store_explicit(&tgp->usrPtr, usrPtr, memory_order_release);
+	atomic_store_explicit(&tgp->count, 0, memory_order_release);
+	atomic_store_explicit(&tgp->status, tg_launching, memory_order_release);
 	return tgp;
 }
 
 int tg_increment(terminationgroup_ptr_t tg) {
-	uint8_t status = atomic_load_explicit(&tg->status, memory_order_acquire);
-	int retval;
-	if (status == launching) {
-		tg->count += 1;
-		retval = 0;
-	} else {
-		retval = 1;
+	switch (atomic_load_explicit(&tg->status, memory_order_acquire)) {
+		case tg_launching:
+			atomic_fetch_add_explicit(&tg->count, 1, memory_order_acq_rel);
+			return 0;
+		default:
+			return 1;
 	}
-	atomic_store_explicit(&tg->status, status, memory_order_release);
-	return retval;
 }
 
 int tg_decrement(terminationgroup_ptr_t tg) {
+	uint oldCount = atomic_fetch_sub_explicit(&tg->count, 1, memory_order_acq_rel);
 	uint8_t status = atomic_load_explicit(&tg->status, memory_order_acquire);
-	tg->count -= 1;
-	
 	// fire the termination handler if the count is 0 and the status is exited
-	if (tg->count == 0 && ((status == exited) || (status == signaled))) {
-		tg->th(tg->usrPtr, tg->pid, tg->exitCode);
+	if (oldCount == 1 && ((status == tg_exited) || (status == tg_signaled) || (status == tg_aborted))) {
+		usr_ptr_t usrPtr = atomic_load_explicit(&tg->usrPtr, memory_order_acquire);
+		int exitc = atomic_load_explicit(&tg->exitCode, memory_order_acquire);
+		tg->th(usrPtr, tg->pid, status, exitc);
+		tg_dealloc(tg);
 	}
-	atomic_store_explicit(&tg->status, status, memory_order_release);
 	return 0;
 }
 
 int tg_launch(terminationgroup_ptr_t tg, pid_t newpid) {
-	uint8_t status = atomic_load_explicit(&tg->status, memory_order_acquire);
-	int retval;
-	if (status == launching) {
-		tg->pid = newpid;
-		status = running;
-		retval = 0;
-	} else {
-		retval = 1;
+	switch (atomic_load_explicit(&tg->status, memory_order_acquire)) {
+		case tg_launching:
+			tg->pid = newpid;
+			atomic_store_explicit(&tg->status, tg_running, memory_order_release);
+			return 0;
+		default:
+			return 1;
 	}
-	atomic_store_explicit(&tg->status, status, memory_order_release);
-	return retval;
 }
 
 int tg_exit(terminationgroup_ptr_t tg, int exitCode) {
-	uint8_t status = atomic_load_explicit(&tg->status, memory_order_acquire);
-	int retval;
-	if (status == running) {
-		tg->exitCode = exitCode;
-		status = exited;
-		retval = 0;
-		// fire the termination handler if the count is 0
-		if (tg->count == 0) {
-			tg->th(tg->usrPtr, tg->pid, exitCode);
-			tg_dealloc(tg);
-			return retval;
-		}
-	} else {
-		retval = 1;
+	switch (atomic_load_explicit(&tg->status, memory_order_acquire)) {
+		case tg_running:
+			atomic_store_explicit(&tg->status, tg_exited, memory_order_release);
+			atomic_store_explicit(&tg->exitCode, exitCode, memory_order_release);
+			if (atomic_load_explicit(&tg->count, memory_order_acquire) == 0) {
+				usr_ptr_t usrPtr = atomic_load_explicit(&tg->usrPtr, memory_order_acquire);
+				tg->th(usrPtr, tg->pid, tg_exited, exitCode);
+				tg_dealloc(tg);
+				return 0;
+			}
+			break;
+		default:
+			return 1;
+			break;
 	}
-	atomic_store_explicit(&tg->status, status, memory_order_release);
-	return retval;
 }
 
 int tg_signal(terminationgroup_ptr_t tg, int signalCode) {
-	uint8_t status = atomic_load_explicit(&tg->status, memory_order_acquire);
-	int retval;
-	if (status == running) {
-		tg->exitCode = signalCode;
-		status = signaled;
-		retval = 0;
-		// fire the termination handler if the count is 0
-		if (tg->count == 0) {
-			tg->th(tg->usrPtr, tg->pid, signalCode);
-			tg_dealloc(tg);
-			return retval;
-		}
-	} else {
-		retval = 1;
+	switch (atomic_load_explicit(&tg->status, memory_order_acquire)) {
+		case tg_running:
+			atomic_store_explicit(&tg->status, tg_signaled, memory_order_release);
+			atomic_store_explicit(&tg->exitCode, signalCode, memory_order_release);
+			if (atomic_load_explicit(&tg->count, memory_order_acquire) == 0) {
+				usr_ptr_t usrPtr = atomic_load_explicit(&tg->usrPtr, memory_order_acquire);
+				tg->th(usrPtr, tg->pid, tg_signaled, signalCode);
+				tg_dealloc(tg);
+				return 0;
+			}
+			break;
+		default:
+			return 1;
+			break;
 	}
-	atomic_store_explicit(&tg->status, status, memory_order_release);
-	return retval;
+}
+
+int tg_abort(terminationgroup_ptr_t tg) {
+	uint8_t getStatus = atomic_load_explicit(&tg->status, memory_order_acquire);
+	if ((getStatus == tg_launching) || (getStatus == tg_running)) {
+		atomic_store_explicit(&tg->status, tg_aborted, memory_order_release);
+	} else {
+		return 1;
+	}
+	
+	if (atomic_load_explicit(&tg->count, memory_order_acquire) == 0) {
+		usr_ptr_t usrPtr = atomic_load_explicit(&tg->usrPtr, memory_order_acquire);
+		tg->th(usrPtr, 0, tg_aborted, 0);
+		tg_dealloc(tg);
+	}
+	return 0;
 }
 
 void*_Nullable tg_pid_ptr(terminationgroup_ptr_t tg) {
 	uint8_t status = atomic_load_explicit(&tg->status, memory_order_acquire);
-	void* retval;
-	if (status != launching) {
-		retval = &tg->pid;
+	if (status == tg_launching || status == tg_aborted) {
+		return NULL;
 	} else {
-		retval = NULL;
+		return &tg->pid;
 	}
-	atomic_store_explicit(&tg->status, status, memory_order_release);
-	return retval;
 }
 
 int tg_dealloc(terminationgroup_ptr_t tg) {
