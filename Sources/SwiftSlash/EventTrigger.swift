d46587b8044cd6dc1b4a13bba31ec91ff350bf0f
tannerdsilva <t@tannersilva.com>
2024-06-23 20:09:58 -0500

    wip

--- Sources/SwiftSlash/EventTrigger.swift
+++ Sources/SwiftSlash/EventTrigger.swift
@@ -150,72 +150,74 @@ public protocol EventTrigger {
 // #elseif os(macOS)
 import Glibc
 // swiftslash is using pthread for the runtime loop that captures events from the kernel. this is so that the thread can be canceled while also using an indefinite wait time in the system polling call.
-// internal struct PThread:~Copyable {
-// 	/// thrown when a pthread cannot be created.
-// 	internal struct LaunchError:Swift.Error {}
+internal struct PThread:~Copyable {
+	/// thrown when a pthread cannot be created.
+	internal struct LaunchError:Swift.Error {}
 
-// 	/// main function that handles the unwrapping of the logger before calling the main function.
-// 	private static func mainWrapper(_ ptr:UnsafeMutableRawPointer) -> UnsafeMutableRawPointer? {
-// 		let unmanaged = Unmanaged<PThread.ContainedConfiguration>.fromOpaque(ptr)
-// 		var logger = unmanaged.takeUnretainedValue().getLogger()
-// 		let tid = pthread_self()
-// 		logger[metadataKey:"pthread_id"] = "\(tid)"
-// 		logger.debug("pthread launched")
-// 		do {
-// 			try unmanaged.takeUnretainedValue().getRunFunction()(logger)
-// 		} catch is CancellationError {
-// 			logger.error("pthread canceled")
-// 		} catch {
-// 			logger.error("pthread error: \(error)")
-// 		}
-// 		pthread_exit(unmanaged.toOpaque())
-// 	}
+	/// main function that handles the unwrapping of the logger before calling the main function.
+	private static func mainWrapper(_ ptr:UnsafeMutableRawPointer) -> UnsafeMutableRawPointer? {
+		let unmanaged = Unmanaged<PThread.ContainedConfiguration>.fromOpaque(ptr)
+		var logger = unmanaged.takeUnretainedValue().getLogger()
+		let tid = pthread_self()
+		logger[metadataKey:"pthread_id"] = "\(tid)"
+		logger.debug("pthread launched")
+		do {
+			try unmanaged.takeUnretainedValue().getRunFunction()(logger)
+		} catch is CancellationError {
+			logger.error("pthread canceled")
+		} catch {
+			logger.error("pthread error: \(error)")
+		}
+		pthread_exit(unmanaged.toOpaque())
+	}
 
-// 	/// the pthread primitive.
-// 	private let pt_primitive:UnsafeMutablePointer<pthread_t>
+	/// the pthread primitive.
+	private let pt_primitive:UnsafeMutablePointer<pthread_t>
 
-// 	internal init(logger:consuming Logger, _ f:@escaping (borrowing Logger) throws -> Void) throws {
-// 		var pthread = pthread_t()
-// 		let configPtr = Unmanaged.passRetained(PThread.ContainedConfiguration(logger, run:f))
-// 		guard pthread_create(&pthread, nil, { Self.mainWrapper($0!) }, configPtr.toOpaque()) != 0 && pthread != nil else {
-// 			_ = configPtr.takeRetainedValue()
-// 			throw LaunchError()
-// 		}
-// 		self.pt_primitive = pthread
-// 	}
+	internal init(logger:consuming Logger, _ f:@escaping (borrowing Logger) throws -> Void) throws {
+		let pthread = UnsafeMutablePointer<pthread_t>.allocate(capacity:1)
+		let configPtr = Unmanaged.passRetained(PThread.ContainedConfiguration(logger, run:f))
+		guard pthread_create(pthread, nil, { Self.mainWrapper($0!) }, configPtr.toOpaque()) != 0 && pthread != nil else {
+			_ = configPtr.takeRetainedValue()
+			throw LaunchError()
+		}
+		self.pt_primitive = pthread
+	}
 
-// 	internal consuming  func cancel() {
-// 		guard pthread_cancel(pt_primitive) == 0 else {
-// 			fatalError("pthread_cancel error \(errno) from \(#file):\(#line)")
-// 		}
-// 	}
 
-// 	deinit {
-// 		var retPtr:UnsafeMutableRawPointer? = nil
-// 		guard pthread_join(pt_primitive, &retPtr) == 0 else {
-// 			fatalError("pthread_join error \(errno) from \(#file):\(#line)")
-// 		}
-// 		_ = Unmanaged<PThread.ContainedConfiguration>.fromOpaque(retPtr!).takeRetainedValue()
-// 	}
-// }
 
-// extension PThread {
-// 	/// this class is used to pass a logger to the pthread in a relatively swift-friedly way.
-// 	internal final class ContainedConfiguration {
-// 		private let logger:Logger
-// 		private let runFunc:(borrowing Logger) throws -> Void
-// 		internal init(_ logger:Logger, run runFunc:@escaping (borrowing Logger) throws -> Void) {
-// 			self.logger = logger
-// 			self.runFunc = runFunc
-// 		}
-// 		internal func getLogger() -> Logger {
-// 			return logger
-// 		}
-// 		internal func getRunFunction() -> (borrowing Logger) throws -> Void {
-// 			return runFunc
-// 		}
-// 	}
-// }
+	internal consuming func cancel() {
+		guard pthread_cancel(pt_primitive.pointee) == 0 else {
+			fatalError("pthread_cancel error \(errno) from \(#file):\(#line)")
+		}
+	}
+
+	deinit {
+		var retPtr:UnsafeMutableRawPointer? = nil
+		guard pthread_join(pt_primitive.pointee, &retPtr) == 0 else {
+			fatalError("pthread_join error \(errno) from \(#file):\(#line)")
+		}
+		_ = Unmanaged<PThread.ContainedConfiguration>.fromOpaque(retPtr!).takeRetainedValue()
+	}
+}
+
+extension PThread {
+	/// this class is used to pass a logger to the pthread in a relatively swift-friedly way.
+	internal final class ContainedConfiguration {
+		private let logger:Logger
+		private let runFunc:(borrowing Logger) throws -> Void
+		internal init(_ logger:Logger, run runFunc:@escaping (borrowing Logger) throws -> Void) {
+			self.logger = logger
+			self.runFunc = runFunc
+		}
+		internal func getLogger() -> Logger {
+			return logger
+		}
+		internal func getRunFunction() -> (borrowing Logger) throws -> Void {
+			return runFunc
+		}
+	}
+}
 
 // internal struct EventTrigger:~Copyable {
 // 	internal typealias QueueType = Int32
