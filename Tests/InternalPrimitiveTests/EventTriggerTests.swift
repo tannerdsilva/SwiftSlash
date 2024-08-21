8be22658835ac0a27a9b88057c97180530ecb1b4
tannerdsilva <t@tannersilva.com>
2024-08-21 16:33:51 -0500

    new test

--- Tests/InternalPrimitiveTests/EventTriggerTests.swift
+++ Tests/InternalPrimitiveTests/EventTriggerTests.swift
@@ -1,14 +1,22 @@
 import XCTest
 @testable import SwiftSlash
+import SwiftSlashEventTrigger
+import SwiftSlashFHHelpers
 
 class EventTriggerTests: XCTestCase {
-	// func testRegisterReader() throws {
-	// 	let eventTrigger = try EventTrigger()
-	// 	let reader: Int32 = 123
-	// 	try eventTrigger.register(reader: reader)
-	// 	// Assert that the reader is registered successfully
-	// 	// You can add your own assertions here
-	// }
+	func testRegisterReader() async throws {
+		let eventTrigger = try await EventTrigger()
+		let readerPipe = try PosixPipe(nonblockingReads:true, nonblockingWrites:false)
+		let readerFIFO = EventTrigger.ReaderFIFO()
+		let myReaderIterator = readerFIFO.makeAsyncConsumer()
+		try eventTrigger.register(reader:readerPipe.reading, readerFIFO)
+		XCTAssertEqual(try readerPipe.writing.writeFH([UInt8]("Hello, World!".utf8)), 13)
+		var captured:[UInt8]? = nil
+		thisThing: while let next = try await myReaderIterator.next() {
+			XCTAssertEqual(next, 13)
+			break thisThing
+		}
+	}
 	
 	// func testRegisterWriter() throws {
 	// 	let eventTrigger = try EventTrigger()
