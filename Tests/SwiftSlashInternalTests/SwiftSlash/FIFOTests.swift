a746d68e7657ae3235850facef43deaebcb492de
tannerdsilva <t@tannersilva.com>
2025-02-08 16:38:50 -0600

    refactored and reenabled fifo tests for swift native testing

--- Tests/SwiftSlashInternalTests/SwiftSlash/FIFOTests.swift
+++ Tests/SwiftSlashInternalTests/SwiftSlash/FIFOTests.swift
@@ -15,8 +15,10 @@ extension SwiftSlashTests {
 			let foundElements = await withTaskGroup(of:[Int].self, returning:[Int].self) { tg in
 				tg.addTask { [asc = fifo!.makeAsyncConsumer()] in
 					var buildInts = [Int]()
-					while let nextElement = await asc.next() {
-						buildInts.append(nextElement.value)
+					var nextElement:WhenDeinitTool<Int>? = await asc.next()
+					while nextElement != nil {
+						buildInts.append(nextElement!.value)
+						nextElement = await asc.next()
 					}
 					return buildInts
 				}
@@ -30,71 +32,71 @@ extension SwiftSlashTests {
 			}
 			#expect(foundElements == [1, 2, 3, 4, 5])
 
-			// Ensure all elements were deinitialized properly
+			// ensure all elements were deinitialized properly
 			#expect(deinitCount == 5)
 		}
 
-	// 	func testNoConsumption() async {
-	// 		// now test the same scenario without any consumption. ensure that the references are deinitialized properly when the fifo is deinitialized.
-	// 		var fifo:FIFO<WhenDeinitTool<Int>, Never>? = FIFO<WhenDeinitTool<Int>, Never>()
-	// 		var deinitCount = 0
-	// 		func didDeinit() {
-	// 			deinitCount += 1
-	// 		}
+		@Test("SwiftSlashFIFO :: basic usage for no consumption", .timeLimit(.minutes(1)))
+		func testNoConsumption() async {
+			// now test the same scenario without any consumption. ensure that the references are deinitialized properly when the fifo is deinitialized.
+			var fifo:FIFO<WhenDeinitTool<Int>, Never>? = FIFO<WhenDeinitTool<Int>, Never>()
+			var deinitCount = 0
+			func didDeinit() {
+				deinitCount += 1
+			}
 
-	// 		fifo!.yield(WhenDeinitTool(1, deinitClosure: didDeinit))
-	// 		fifo!.yield(WhenDeinitTool(2, deinitClosure:didDeinit))
-	// 		fifo!.yield(WhenDeinitTool(3, deinitClosure: didDeinit))
-	// 		fifo!.yield(WhenDeinitTool(4, deinitClosure: didDeinit))
-	// 		fifo!.yield(WhenDeinitTool(5, deinitClosure: didDeinit))
+			fifo!.yield(WhenDeinitTool(1, deinitClosure: didDeinit))
+			fifo!.yield(WhenDeinitTool(2, deinitClosure:didDeinit))
+			fifo!.yield(WhenDeinitTool(3, deinitClosure: didDeinit))
+			fifo!.yield(WhenDeinitTool(4, deinitClosure: didDeinit))
+			fifo!.yield(WhenDeinitTool(5, deinitClosure: didDeinit))
 
-	// 		// Deinitialize the fifo
-	// 		fifo = nil
+			// Deinitialize the fifo
+			fifo = nil
 
-	// 		// Ensure all elements were deinitialized properly
-	// 		XCTAssertEqual(deinitCount, 5)
-	// 	}
-	// 	func testPartialConsumption() async {
-	// 		// test a partial consumption scenario. ensure that the references are deinitialized properly when the fifo is deinitialized.
-	// 		var fifo:FIFO<WhenDeinitTool<Int>, Never>? = FIFO<WhenDeinitTool<Int>, Never>()
-	// 		var deinitCount = 0
-	// 		func didDeinit() {
-	// 			deinitCount += 1
-	// 		}
+			// Ensure all elements were deinitialized properly
+			#expect(deinitCount == 5)
+		}
+		@Test("SwiftSlashFIFO :: memory management in partial consumption scenario", .timeLimit(.minutes(1)))
+		func testPartialConsumption() async {
+			// test a partial consumption scenario. ensure that the references are deinitialized properly when the fifo is deinitialized.
+			var fifo:FIFO<WhenDeinitTool<Int>, Never>? = FIFO<WhenDeinitTool<Int>, Never>()
+			var deinitCount = 0
+			func didDeinit() {
+				deinitCount += 1
+			}
+			
+			await withTaskGroup(of:[Int].self) { tg in
+				tg.addTask { [asc = fifo!.makeAsyncConsumer()] in
+					var buildInts = [Int]()
+					var nextElement:WhenDeinitTool<Int>? = await asc.next()
+					while nextElement != nil {
+						buildInts.append(nextElement!.value)
+						if buildInts.count == 3 {
+							break
+						} else {
+							nextElement = await asc.next()
+						}
+					}
+					return buildInts
+				}
+				fifo!.yield(WhenDeinitTool(1, deinitClosure: didDeinit))
+				fifo!.yield(WhenDeinitTool(2, deinitClosure: didDeinit))
+				fifo!.yield(WhenDeinitTool(3, deinitClosure: didDeinit))
+				fifo!.yield(WhenDeinitTool(4, deinitClosure: didDeinit))
+				fifo!.yield(WhenDeinitTool(5, deinitClosure: didDeinit))
+				fifo!.finish()
+				await tg.waitForAll()
+			}
 
-	// 		var fifoIterator:FIFO<WhenDeinitTool<Int>, Never>.AsyncConsumer? = fifo!.makeAsyncConsumer()
-	// 		let consumer = Task { [fi = fifoIterator!] in
-	// 			var fifoIterator = fi
-	// 			do {
-	// 				var result: [Int] = []
-	// 				while let element = try await fifoIterator.next() {
-	// 					result.append(element.value)  // Access the value stored within WhenDeinitTool
-	// 					if result.count == 3 {
-	// 						break
-	// 					}
-	// 				}
-	// 				XCTAssertEqual(result, [1, 2, 3])
-	// 			} catch {
-	// 				XCTFail("Error consuming data: \(error)")
-	// 			}
-	// 		}
+			#expect(deinitCount == 3)
 
-	// 		fifo!.yield(WhenDeinitTool(1, deinitClosure: didDeinit))
-	// 		fifo!.yield(WhenDeinitTool(2, deinitClosure: didDeinit))
-	// 		fifo!.yield(WhenDeinitTool(3, deinitClosure: didDeinit))
-	// 		fifo!.yield(WhenDeinitTool(4, deinitClosure: didDeinit))
-	// 		fifo!.yield(WhenDeinitTool(5, deinitClosure: didDeinit))
+			// Deinitialize the fifo
+			fifo = nil
+			// fifoIterator = nil
 
-	// 		await consumer.result
-
-	// 		XCTAssertEqual(deinitCount, 3)
-
-	// 		// Deinitialize the fifo
-	// 		fifo = nil
-	// 		fifoIterator = nil
-
-	// 		// Ensure all elements were deinitialized properly
-	// 		XCTAssertEqual(deinitCount, 5)
-	// 	}
+			// Ensure all elements were deinitialized properly
+			#expect(deinitCount == 5)
+		}
 	}
 }
