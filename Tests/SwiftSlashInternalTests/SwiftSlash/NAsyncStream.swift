fd81521f84761411e5e29b81e58bc7227ab12740
tannerdsilva <t@tannersilva.com>
2025-02-25 09:20:29 -0600

    fixing things

--- Tests/SwiftSlashInternalTests/SwiftSlash/NAsyncStream.swift
+++ Tests/SwiftSlashInternalTests/SwiftSlash/NAsyncStream.swift
@@ -10,7 +10,7 @@ extension SwiftSlashTests {
 			await confirmation("confirm correct memory management throughout lifecycle", expectedCount:3) { deinitConf in
 				await withTaskGroup(of:Void.self) { tg in
 					tg.addTask { [asT = stream.makeAsyncConsumer()] in
-						var result: [Int] = []
+						var result:[Int] = []
 						while let item = await asT.next(whenTaskCancelled:.noAction) {
 							result.append(item.value)
 						}
@@ -19,6 +19,28 @@ extension SwiftSlashTests {
 					stream.yield(WhenDeinitTool(1, deinitConf))
 					stream.yield(WhenDeinitTool(2, deinitConf))
 					stream.yield(WhenDeinitTool(3, deinitConf))
+					stream.finish()
+					await tg.waitForAll()
+				}
+			}
+		}
+
+		@Test("SwiftSlashNAsyncStream :: testing large number of elements")
+		func testNasyncStreamLargeElementCount() async {
+			let elementCount = 10000
+			let stream = NAsyncStream<WhenDeinitTool<Int>, Never>()
+			await confirmation("confirm correct memory management throughout lifecycle", expectedCount:elementCount) { deinitConf in
+				await withTaskGroup(of:Void.self) { tg in
+					tg.addTask { [asT = stream.makeAsyncConsumer()] in
+						var result:[Int] = []
+						while let item = await asT.next(whenTaskCancelled:.noAction) {
+							result.append(item.value)
+						}
+						#expect(result == Array(1...elementCount))
+					}
+					for i in 1...elementCount {
+						stream.yield(WhenDeinitTool(i, deinitConf))
+					}
 					stream.finish()
 					await tg.waitForAll()
 				}
