800d9ba9344d5c3457174e547a9cdd428b9e2028
tannerdsilva <t@tannersilva.com>
2025-04-23 12:54:41 -0500

    wip linux eventtrigger

--- Sources/SwiftSlashEventTrigger/Platforms/LinuxEventTrigger.swift
+++ Sources/SwiftSlashEventTrigger/Platforms/LinuxEventTrigger.swift
@@ -1,8 +1,20 @@
 #if os(Linux)
-import __cswiftslash
+/*
+LICENSE MIT
+copyright (c) tanner silva 2025. all rights reserved.
+
+   _____      ______________________   ___   ______ __
+  / __/ | /| / /  _/ __/_  __/ __/ /  / _ | / __/ // /
+ _\ \ | |/ |/ // // _/  / / _\ \/ /__/ __ |_\ \/ _  / 
+/___/ |__/|__/___/_/   /_/ /___/____/_/ |_/___/_//_/  
+
+*/
+
+#if os(macOS)
+import __cswiftslash_eventtrigger
 import SwiftSlashFIFO
 import SwiftSlashPThread
-
+import SwiftSlashFHHelpers
 
 internal final class LinuxEventTrigger:EventTriggerEngine {
 	internal typealias RuntimeErrors = Never
@@ -10,10 +22,13 @@ internal final class LinuxEventTrigger:EventTriggerEngine {
 	internal typealias ArgumentType = EventTriggerSetup<EventTriggerHandle>
 	internal typealias ReturnType = Void
 	internal typealias EventTriggerHandle = Int32
-	internal typealias EventType = epoll_event
+	internal typealias EventType = kevent
 
 	/// the event trigger primitive
 	internal let prim:EventTriggerHandlePrimitive
+
+	// the pipe that is used to cancel the event trigger.
+	internal let cancelPipe:PosixPipe
 
 	/// stores the fifo's that read data is passed into.
 	private var readersDataOut:[Int32:FIFO<size_t, Never>] = [:]
@@ -43,6 +58,7 @@ internal final class LinuxEventTrigger:EventTriggerEngine {
 	internal init(_ ptSetup:consuming ArgumentType) {
 		registrations = ptSetup.registersIn
 		prim = ptSetup.handle
+		cancelPipe = ptSetup.cancelPipe
 	}
 
 	/// event buffer that allows us to process events. this buffer is passed directly to the system call and is the first place returned events are stored.
@@ -63,9 +79,8 @@ internal final class LinuxEventTrigger:EventTriggerEngine {
 		repeat {
 
 			// wait for events. this might block.
-			let kqueueResult = kevent(prim, nil, 0, eventBuffer, eventBufferSize, nil)
-
-			switch kqueueResult {
+			let epollResult = kevent(prim, nil, 0, eventBuffer, eventBufferSize, nil)
+			switch epollResult {
 				// abnormal error conditions.
 				case Int32.min..<0:
 					switch errno {
@@ -87,6 +102,12 @@ internal final class LinuxEventTrigger:EventTriggerEngine {
 						let currentEvent = eventBuffer[i]
 						// capture the file handle that associates with the event.
 						let curIdent = Int32(currentEvent.ident)
+						// verify that this identifier is not the reading end of the cancel pipe.
+						guard curIdent != cancelPipe.reading else {
+							// skip the cancel identifier because it does not register with the event trigger so there is nothing to pass on.
+							continue resultLoop
+						}
+
 						// logic branch to determine if the event is a read or write event, or if it is an EOF event.
 						if currentEvent.flags & UInt16(EV_EOF) == 0 {
 							if currentEvent.filter == Int16(EVFILT_READ) {
@@ -128,7 +149,7 @@ internal final class LinuxEventTrigger:EventTriggerEngine {
 	}
 
 	internal static func newHandlePrimitive() -> EventTriggerHandle {
-		return kqueue()
+		return epoll_create1(0)
 	}
 
 	internal static func closePrimitive(_ prim:consuming EventTriggerHandle) {
@@ -136,6 +157,43 @@ internal final class LinuxEventTrigger:EventTriggerEngine {
 	}
 }
 
+extension MacOSEventTrigger {
+	internal static func register(_ ev:EventTriggerHandlePrimitive, reader:Int32) throws(EventTriggerErrors) {
+		var newEvent = epoll_event()
+		newEvent.data.fd = reader
+		newEvent.events = UInt32(EPOLLIN.rawValue) | UInt32(EPOLLERR.rawValue) | UInt32(EPOLLHUP.rawValue) | UInt32(EPOLLET.rawValue)
+		guard epoll_ctl(ev, EPOLL_CTL_ADD, reader, &newEvent) == 0 else {
+			throw RuntimeErrors.readerRegistrationFailure
+		}
+	}
+
+	internal static func register(_ ev:EventTriggerHandlePrimitive, writer:Int32) throws(EventTriggerErrors) {
+		var newEvent = epoll_event()
+		newEvent.data.fd = writer
+		newEvent.events = UInt32(EPOLLOUT.rawValue) | UInt32(EPOLLERR.rawValue) | UInt32(EPOLLHUP.rawValue) | UInt32(EPOLLET.rawValue)
+		guard epoll_ctl(ev, EPOLL_CTL_ADD, writer, &newEvent) == 0 else {
+			throw RuntimeErrors.writerRegistrationFailure
+		}
+	}
+
+	internal static func deregister(_ ev:EventTriggerHandlePrimitive, reader:Int32) throws(EventTriggerErrors) {
+		var buildEvent = epoll_event()
+		buildEvent.data.fd = reader
+		buildEvent.events = UInt32(EPOLLIN.rawValue) | UInt32(EPOLLERR.rawValue) | UInt32(EPOLLHUP.rawValue) | UInt32(EPOLLET.rawValue)
+		guard epoll_ctl(ev, EPOLL_CTL_DEL, reader, &buildEvent) == 0 else {
+			throw RuntimeErrors.readerDeregistrationFailure
+		}
+	}
+
+	internal static func deregister(_ ev:EventTriggerHandlePrimitive, writer:Int32) throws(EventTriggerErrors) {
+		var buildEvent = epoll_event()
+		buildEvent.data.fd = writer
+		buildEvent.events = UInt32(EPOLLOUT.rawValue) | UInt32(EPOLLERR.rawValue) | UInt32(EPOLLHUP.rawValue) | UInt32(EPOLLET.rawValue)
+		guard epoll_ctl(ev, EPOLL_CTL_DEL, writer, &buildEvent) == 0 else {
+			throw RuntimeErrors.writerDeregistrationFailure
+		}
+	}
+}
 
 /*internal final class LinuxET:EventTriggerEngine {
 
