50d2148760809537329122ebfbfe7d735182a897
tannerdsilva <t@tannersilva.com>
2026-07-08 17:10:48 -0500

    wip

--- Sources/SwiftSlashGlobalSerialization/GlobalSerialization.swift
+++ Sources/SwiftSlashGlobalSerialization/GlobalSerialization.swift
@@ -9,10 +9,6 @@ copyright (c) tanner silva 2025. all rights reserved.
 
 */
 
-import SwiftSlashPThreadSerialExecutor
-import SwiftSlashFIFO
-import SwiftSlashPThread
-
 /// a process may only launch one child process at a time. no step in regards to producing a child process is reentrant safe. this actor enforces this strictly.
 @globalActor public actor SwiftSlashGlobalSerialization:GlobalActor {
 	/// the global actor that is used to serialize the launch of child processes.
